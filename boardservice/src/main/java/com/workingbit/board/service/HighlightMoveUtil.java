361b5ebb80de73c6185c9f10b4aca6c57867bbca
hello-alexpo <aleksey.popryaduhin@yahoo.com>
2017-08-30 14:18:17 +0300

    Polish

--- boardservice/src/main/java/com/workingbit/board/service/HighlightMoveUtil.java
+++ boardservice/src/main/java/com/workingbit/board/service/HighlightMoveUtil.java
@@ -1,7 +1,6 @@
 package com.workingbit.board.service;
 
 import com.workingbit.board.exception.BoardServiceException;
-import com.workingbit.share.common.EnumRules;
 import com.workingbit.share.domain.impl.Board;
 import com.workingbit.share.domain.impl.BoardContainer;
 import com.workingbit.share.domain.impl.Draught;
@@ -17,38 +16,39 @@ import static com.workingbit.board.service.BoardUtils.isSubDiagonal;
 /**
  * Created by Aleksey Popryaduhin on 19:39 10/08/2017.
  */
-public class HighlightMoveUtil {
+class HighlightMoveUtil {
 
-  /**
-   * possible directions of moving
-   */
-  private BoardContainer board;
   private Square selectedSquare;
-  private EnumRules rules;
 
-  private HighlightMoveUtil(BoardContainer board, Square selectedSquare, EnumRules rules) throws BoardServiceException {
+  private HighlightMoveUtil(BoardContainer board, Square selectedSquare) throws BoardServiceException {
     if (selectedSquare == null || selectedSquare.getDraught() == null) {
       throw new BoardServiceException("Selected square without placed draught");
     }
-    this.board = board;
     this.selectedSquare = selectedSquare;
-    this.rules = rules;
     selectedSquare.getDraught().setHighlighted(true);
     board.setSelectedSquare(selectedSquare);
   }
 
+  static Optional<Map<String, Object>> highlight(Board board, Square selectedSquare) throws BoardServiceException, ExecutionException, InterruptedException {
+    try {
+      // highlight moves for the selected square
+      HighlightMoveUtil highlightMoveUtil = new HighlightMoveUtil(board.getCurrentBoard(), selectedSquare);
+      return Optional.of(highlightMoveUtil.findAllMoves());
+    } catch (BoardServiceException e) {
+      return Optional.empty();
+    }
+  }
+
   /**
    * Entry point for initially selected square
-   *
-   * @return
    */
-  public Map<String, Object> findAllMoves() throws BoardServiceException {
+  private Map<String, Object> findAllMoves() throws BoardServiceException {
     List<Square> allowedMoves = new ArrayList<>();
     List<Square> beatenMoves = new ArrayList<>();
     Draught draught = selectedSquare.getDraught();
     boolean black = draught.isBlack();
     boolean queen = draught.isQueen();
-    findBeatenMovesOnDiagonalsOfSelectedSquare(selectedSquare, beatenMoves, black, queen, 0);
+    findBeatenMovesOnDiagonalsOfSelectedSquare(selectedSquare, beatenMoves, black, queen);
     if (beatenMoves.isEmpty()) {
       findAllowedMoves(selectedSquare, allowedMoves, black, queen);
     }
@@ -67,15 +67,15 @@ public class HighlightMoveUtil {
         if (!queen) {
           findAllowed(allowedMoves, black, squareListIterator);
         } else {
-          findAllowedForQueen(selectedSquare, allowedMoves, black, squareListIterator);
+          findAllowedForQueen(allowedMoves, black, squareListIterator);
           squareListIterator = diagonal.listIterator(indexOfSelected);
-          findAllowedForQueen(selectedSquare, allowedMoves, !black, squareListIterator);
+          findAllowedForQueen(allowedMoves, !black, squareListIterator);
         }
       }
     }
   }
 
-  private void findAllowedForQueen(Square selectedSquare, List<Square> allowedMoves, boolean black, ListIterator<Square> squareListIterator) {
+  private void findAllowedForQueen(List<Square> allowedMoves, boolean black, ListIterator<Square> squareListIterator) {
     do {
       findAllowed(allowedMoves, black, squareListIterator);
     } while (black && squareListIterator.hasNext() || !black && squareListIterator.hasPrevious());
@@ -88,26 +88,30 @@ public class HighlightMoveUtil {
     }
   }
 
-  private void findBeatenMovesOnDiagonalsOfSelectedSquare(Square selectedSquare, List<Square> beatenMoves, boolean black, boolean queen, int deep) throws BoardServiceException {
+  private void findBeatenMovesOnDiagonalsOfSelectedSquare(Square selectedSquare, List<Square> beatenMoves, boolean black, boolean queen) throws BoardServiceException {
     List<List<Square>> diagonals = selectedSquare.getDiagonals();
     int indexOfSelected;
     for (List<Square> diagonal : diagonals) {
       indexOfSelected = diagonal.indexOf(selectedSquare);
       if (indexOfSelected != -1) {
         List<Square> newBeaten = new ArrayList<>();
-        walkOnDiagonal(selectedSquare, newBeaten, black, queen, deep, diagonal);
+        walkOnDiagonal(selectedSquare, newBeaten, black, queen, diagonal);
         beatenMoves.addAll(newBeaten);
       }
     }
   }
 
-  private void walkOnDiagonal(Square selectedSquare, List<Square> beatenMoves, boolean down, boolean queen, int deep, List<Square> diagonal) throws BoardServiceException {
+  private void walkOnDiagonal(Square selectedSquare, List<Square> beatenMoves, boolean down, boolean queen, List<Square> diagonal) throws BoardServiceException {
     ListIterator<Square> squareListIterator;
     int indexOfSelected = diagonal.indexOf(selectedSquare);
     squareListIterator = diagonal.listIterator(indexOfSelected);
-    findBeatenMovesOnHalfDiagonal(beatenMoves, squareListIterator, selectedSquare, down, queen, deep);
+    findBeatenMovesOnHalfDiagonal(beatenMoves, squareListIterator, selectedSquare, down, queen);
     squareListIterator = diagonal.listIterator(indexOfSelected);
-    findBeatenMovesOnHalfDiagonal(beatenMoves, squareListIterator, selectedSquare, !down, queen, deep);
+    findBeatenMovesOnHalfDiagonal(beatenMoves, squareListIterator, selectedSquare, !down, queen);
+  }
+
+  private void findBeatenMovesOnHalfDiagonal(List<Square> beatenMoves, ListIterator<Square> squareListIterator, Square selectedSquare, boolean down, boolean queen) throws BoardServiceException {
+    findBeatenMovesOnHalfDiagonal(beatenMoves, squareListIterator, selectedSquare, down, queen, 0);
   }
 
   private void findBeatenMovesOnHalfDiagonal(List<Square> beatenMoves, ListIterator<Square> squareListIterator, Square selectedSquare, boolean down, boolean queen, int deep) throws BoardServiceException {
@@ -131,18 +135,22 @@ public class HighlightMoveUtil {
         return;
       }
       if (!beatenMoves.isEmpty() && deep < 3) {
-        for (List<Square> diagonal : next.getDiagonals()) {
-          if (!isSubDiagonal(diagonal, Arrays.asList(previous, next))) {
-            int indexOfSelected = diagonal.indexOf(next);
-            ListIterator<Square> squareListIteratorDown = diagonal.listIterator(indexOfSelected);
-            findBeatenMovesOnHalfDiagonal(beatenMoves, squareListIteratorDown, next, down, queen, deep);
-            ListIterator<Square> squareListIteratorUp = diagonal.listIterator(indexOfSelected);
-            findBeatenMovesOnHalfDiagonal(beatenMoves, squareListIteratorUp, next, !down, queen, deep);
-          }
-        }
+        walkCrossDiagonal(beatenMoves, down, queen, deep, next, previous);
       }
       previous = next;
     } while ((down && squareListIterator.hasNext()) || (!down && squareListIterator.hasPrevious()));
+  }
+
+  private void walkCrossDiagonal(List<Square> beatenMoves, boolean down, boolean queen, int deep, Square next, Square previous) throws BoardServiceException {
+    for (List<Square> diagonal : next.getDiagonals()) {
+      if (!isSubDiagonal(diagonal, Arrays.asList(previous, next))) {
+        int indexOfSelected = diagonal.indexOf(next);
+        ListIterator<Square> squareListIteratorDown = diagonal.listIterator(indexOfSelected);
+        findBeatenMovesOnHalfDiagonal(beatenMoves, squareListIteratorDown, next, down, queen, deep);
+        ListIterator<Square> squareListIteratorUp = diagonal.listIterator(indexOfSelected);
+        findBeatenMovesOnHalfDiagonal(beatenMoves, squareListIteratorUp, next, !down, queen, deep);
+      }
+    }
   }
 
   private boolean isDraughtWithSameColor(Square next) {
@@ -156,22 +164,12 @@ public class HighlightMoveUtil {
   /**
    * Return allowed square for move after beat
    *
-   * @param nextSquare
-   * @return
+   * @param nextSquare square after previous
+   * @return must or not beat
    */
   private boolean mustBeat(Square nextSquare, Square previousSquare) throws BoardServiceException {
     return previousSquare.isOccupied()
         && previousSquare.getDraught().isBlack() != this.selectedSquare.getDraught().isBlack()
         && !nextSquare.isOccupied();
-  }
-
-  public static Optional<Map<String, Object>> highlight(Board board, Square selectedSquare) throws BoardServiceException, ExecutionException, InterruptedException {
-    try {
-      // highlight moves for the selected square
-      HighlightMoveUtil highlightMoveUtil = new HighlightMoveUtil(board.getCurrentBoard(), selectedSquare, board.getRules());
-      return Optional.of(highlightMoveUtil.findAllMoves());
-    } catch (BoardServiceException e) {
-      return Optional.empty();
-    }
   }
 }
