bdbfc183572dbb20146488a113854bd67eebee82
Aleksey <aleksey.popryaduhin@yahoo.com>
2017-09-26 19:18:09 +0300

    missed api files

---
+++ articleservice/src/swagger/src/main/java/com/workingbit/article/model/Articles.java
@@ -0,0 +1,52 @@
+package com.workingbit.article.model;
+
+import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Articles
+ */
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-09-26T18:31:04.372+03:00")
+
+public class Articles extends ArrayList<com.workingbit.share.domain.impl.Article>  {
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode());
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Articles {\n");
+    sb.append("    ").append(toIndentedString(super.toString())).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
